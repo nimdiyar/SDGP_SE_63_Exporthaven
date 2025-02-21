e5ef0e0bba793ec9ec1f2a9ee242845c5b1f2c27
nimdiyar <146363883+nimdiyar@users.noreply.github.com>
2025-02-21 15:23:46 +0530

    Add Privacy Policy title and introduction

--- Exporthaven_ClientSide/frontend/src/pages/PrivacyPolicy.js
+++ Exporthaven_ClientSide/frontend/src/pages/PrivacyPolicy.js
@@ -2,6 +2,13 @@ import React from "react";
 import "../styles/PrivacyPolicy.css";
 
 const PrivacyPolicy = () => {
+    return(
+        <div className="privacy-policy">
+            <h1>Privacy Policy</h1>
+            <p>We are committed to protecting your personal data and privacy.</p>
 
+        </div>
+
+    );
 };
 export default PrivacyPolicy;
