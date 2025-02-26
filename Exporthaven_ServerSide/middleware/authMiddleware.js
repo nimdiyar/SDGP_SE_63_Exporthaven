c24514af961ad8f4049db1a53a40db24f7607286
nadunJayathunga2330 <nadun.20230306@iit.ac.lk>
2025-02-26 22:59:25 +0530

    Add Authentication & Admin Middleware

---
+++ Exporthaven_ServerSide/middleware/authMiddleware.js
@@ -0,0 +1,9 @@
+const adminAuth = (req, res, next) => {
+    if (!req.user || req.user.role !== "admin") {
+      return res.status(403).json({ message: "Access denied, admin only" });
+    }
+    next();
+  };
+  
+  module.exports = { adminAuth };
+  
