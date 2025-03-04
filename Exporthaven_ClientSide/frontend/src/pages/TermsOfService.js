026b58a33b4cc4330db7d79d6bea434fdca335c6
nimdiyar <146363883+nimdiyar@users.noreply.github.com>
2025-03-04 10:56:58 +0530

    Add basuc TermsOf Service Component Structure

---
+++ Exporthaven_ClientSide/frontend/src/pages/TermsOfService.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/TermsOfService.css";
+
+const TermsOfService = () => {
+    return (
+        <div className="terms-of-service">
+
+        
+        </div>
+    );
+};
+
+export default TermsOfService;
