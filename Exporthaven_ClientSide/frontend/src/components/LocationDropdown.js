ffb4873b8c0c26918003679d41136554cfdeaa34
udayangi-arch <udayangi.20230200@iit.ac.lk>
2025-03-09 09:01:15 +0530

    Added location options to dropdown

--- Exporthaven_ClientSide/frontend/src/components/LocationDropdown.js
+++ Exporthaven_ClientSide/frontend/src/components/LocationDropdown.js
@@ -13,6 +13,31 @@ const LocationDropdown = ({
     onChange={(e) => onChange(e.target.value)}
 >
         <option value="">{placeholder}</option>
+        <option value="Kandy">Kandy</option>
+      <option value="Matale">Matale</option>
+      <option value="Nuwara Eliya">Nuwara Eliya</option>
+      <option value="Ampara">Ampara</option>
+      <option value="Batticaloa">Batticaloa</option>
+      <option value="Trincomalee">Trincomalee</option>
+      <option value="Anuradhapura">Anuradhapura</option>
+      <option value="Polonnaruwa">Polonnaruwa</option>
+      <option value="Jaffna">Jaffna</option>
+      <option value="Kilinochchi">Kilinochchi</option>
+      <option value="Mannar">Mannar</option>
+      <option value="Mullaitivu">Mullaitivu</option>
+      <option value="Vavuniya">Vavuniya</option>
+      <option value="Kurunegala">Kurunegala</option>
+      <option value="Puttalam">Puttalam</option>
+      <option value="Kegalle">Kegalle</option>
+      <option value="Ratnapura">Ratnapura</option>
+      <option value="Galle">Galle</option>
+      <option value="Hambantota">Hambantota</option>
+      <option value="Matara">Matara</option>
+      <option value="Badulla">Badulla</option>
+      <option value="Moneragala">Moneragala</option>
+      <option value="Colombo">Colombo</option>
+      <option value="Gampaha">Gampaha</option>
+      <option value="Kalutara">Kalutara</option>
     </select>
     );
   };
