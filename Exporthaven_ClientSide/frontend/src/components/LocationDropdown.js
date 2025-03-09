123d200e722601010f4d3a10dcf36744ad91bfe1
udayangi-arch <udayangi.20230200@iit.ac.lk>
2025-03-09 08:58:32 +0530

    Added props handling and onChange event

--- Exporthaven_ClientSide/frontend/src/components/LocationDropdown.js
+++ Exporthaven_ClientSide/frontend/src/components/LocationDropdown.js
@@ -3,8 +3,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-const LocationDropdown = () => {
-    return <select></select>;
+const LocationDropdown = ({
+    value,
+    onChange,
+    placeholder = "Select location",
+  }) => {
+    return( 
+         <select value={value}
+    onChange={(e) => onChange(e.target.value)}
+>
+        <option value="">{placeholder}</option>
+    </select>
+    );
   };
   
   export default LocationDropdown;
