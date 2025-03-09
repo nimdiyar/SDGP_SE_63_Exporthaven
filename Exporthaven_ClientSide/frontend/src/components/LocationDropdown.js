5d27cc99890cd8b7933d482aa7a7894e575f48ca
udayangi-arch <udayangi.20230200@iit.ac.lk>
2025-03-09 08:46:19 +0530

    Added LocationDropdown component structure

---
+++ Exporthaven_ClientSide/frontend/src/components/LocationDropdown.js
@@ -0,0 +1,11 @@
+// src/components/LocationDropdown.js
+import React from "react";
+import PropTypes from "prop-types";
+
+
+const LocationDropdown = () => {
+    return <select></select>;
+  };
+  
+  export default LocationDropdown;
+  
