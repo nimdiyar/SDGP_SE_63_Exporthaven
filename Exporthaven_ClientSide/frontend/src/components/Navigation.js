d1dd73dcf930f604473dec1f2a181594db536508
nadunJayathunga2330 <nadun.20230306@iit.ac.lk>
2025-03-13 00:14:45 +0530

    Add navigation component

---
+++ Exporthaven_ClientSide/frontend/src/components/Navigation.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+const Navigation = () => {
+  const { isAuthenticated, logout } = useContext(AuthContext);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {isAuthenticated && (
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        )}
+        {!isAuthenticated ? (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        ) : (
+          <li>
+            <button onClick={logout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
