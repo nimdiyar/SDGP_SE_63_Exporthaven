3dd5c0e7f2427c9796de0b7defb66be020ea5594
Manuljee <vidaravidunadee2003@gmail.com>
2025-03-17 17:15:14 +0530

    Implement Features Section

--- Exporthaven_ClientSide/frontend/src/Pages/AboutUs.js
+++ Exporthaven_ClientSide/frontend/src/Pages/AboutUs.js
@@ -87,6 +87,28 @@ function AboutUs() {
           </p>
         </div>
       </div>
+
+     <div className="features-section">
+        <div className="container">
+          <div className="features-grid">
+            {features.map((feature, index) => (
+              <div key={index} className="feature-card">
+                <div className="icon-container">{feature.icon}</div>
+                <h3 className="feature-title">{feature.title}</h3>
+                <p className="feature-description">{feature.description}</p>
+                <div className="learn-more">
+                  Learn More{" "}
+                  <ArrowRight
+                    className="icon"
+                    style={{ width: "1rem", marginLeft: "0.5rem" }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div> 
+        </div>
+      </div>
+          
     </div>
 
   );
