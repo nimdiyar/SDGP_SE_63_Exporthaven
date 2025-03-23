21ffabee5eb4f78302c5bf645b13c45afe3249cf
Yoshen123 <yoshen.20231476@iit.ac.lk>
2025-03-23 22:05:21 +0530

    After cleaning up and testing the codes

---
+++ Exporthaven_ServerSide/config/db.js
@@ -0,0 +1,16 @@
+//config\db.js
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+export default connectDB;
